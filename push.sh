fdd16abe37814019db76398d337ab883046ea706
sukijo-aja <sukijo.aja@gmail.com>
2026-07-25 11:16:59 +0700

    add products

--- push.sh
+++ push.sh
@@ -6,6 +6,16 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+echo "Generate index file..."
+# Pindah ke direktori dalam subshell agar tidak mengubah working directory utama
+(
+    cd data/muslimapp/products/ || exit
+    # Mengambil semua nama file json kecuali index.json itu sendiri, lalu diformat menjadi array JSON
+    printf "[\n" > index.json
+    ls *.json | grep -v "index.json" | awk '{print "  \""$0"\""}' | paste -sd ",\n" >> index.json
+    printf "\n]\n" >> index.json
+)
+
 echo "Menambahkan perubahan..."
 git add .
 
