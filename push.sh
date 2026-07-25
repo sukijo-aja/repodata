e3776f093d4938b004ae4f8e31423451fb859f8a
sukijo-aja <sukijo.aja@gmail.com>
2026-07-25 11:49:21 +0700

    update index tambah tab khutbah

--- push.sh
+++ push.sh
@@ -10,7 +10,13 @@ echo "Generate index file..."
 # Pindah ke direktori dalam subshell agar tidak mengubah working directory utama
 (
     cd data/muslimapp/products/ || exit
-    # Mengambil semua nama file json kecuali index.json itu sendiri, lalu diformat menjadi array JSON
+    printf "[\n" > index.json
+    ls *.json | grep -v "index.json" | awk '{print "  \""$0"\""}' | paste -sd ",\n" >> index.json
+    printf "\n]\n" >> index.json
+)
+
+(
+    cd data/muslimapp/khutbah/ || exit
     printf "[\n" > index.json
     ls *.json | grep -v "index.json" | awk '{print "  \""$0"\""}' | paste -sd ",\n" >> index.json
     printf "\n]\n" >> index.json
